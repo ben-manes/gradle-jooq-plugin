0a4661086a9f05bf2c9d9120ad4b373d43a47168
Ben Manes <ben.manes@gmail.com>
2013-03-07 13:00:19 -0800

    Added ModelMapper to example usage
    jOOQ provides a nice, but simple mapper between records and models. A
    more complex mapping, such as with nested models, is not supported.
    Instead ModelMapper is used to automatically project and flatten when
    marshaling types.

--- example/src/test/java/example/JooqTest.java
+++ example/src/test/java/example/JooqTest.java
@@ -15,16 +15,22 @@
  */
 package example;
 
+import java.util.List;
+
 import com.google.inject.Inject;
 import example.generated.tables.records.UserRecord;
+import org.jooq.Record;
+import org.jooq.RecordMapper;
 import org.jooq.impl.Executor;
+import org.modelmapper.ModelMapper;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Guice;
 import org.testng.annotations.Test;
 
 import static example.generated.tables.User.USER;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 
 /**
@@ -33,6 +39,17 @@ import static org.hamcrest.Matchers.is;
 @Guice(modules = {JooqModule.class, H2Module.class})
 public final class JooqTest {
   @Inject Executor db;
+  @Inject ModelMapper mapper;
+  RecordMapper<Record, User> recordMapper;
+
+  @BeforeClass
+  public void beforeClass() {
+    recordMapper = new RecordMapper<Record, User>() {
+      @Override public User map(Record record) {
+        return mapper.map(record, User.class);
+      }
+    };
+  }
 
   @AfterMethod
   public void afterMethod() {
@@ -40,24 +57,46 @@ public final class JooqTest {
   }
 
   @Test
-  public void insert() {
-    UserRecord user = insertUser("John", "Doe");
-    assertThat(user.getId(), is(1));
+  public void select() {
+    User john = insertUser("John", "Doe");
+    User user = db.selectFrom(USER)
+        .where(USER.FIRST_NAME.equal("John"))
+        .fetchOne().map(recordMapper);
+    assertThat(user, is(john));
   }
 
   @Test
-  public void select() {
-    UserRecord user = insertUser("John", "Doe");
-    UserRecord john = db.selectFrom(USER)
-        .where(USER.FIRST_NAME.equal("John"))
-        .fetchOne();
-    assertThat(john, is(equalTo(user)));
+  public void selectAll() {
+    User john = insertUser("John", "Doe");
+    User jane = insertUser("Jane", "Doe");
+    List<User> users = db.selectFrom(USER).fetch().map(recordMapper);
+    assertThat(users, contains(john, jane));
   }
 
-  private UserRecord insertUser(String firstName, String lastName) {
-    UserRecord user = new UserRecord();
-    user.setFirstName(firstName);
-    user.setLastName(lastName);
-    return db.insertInto(USER).set(user).returning().fetchOne();
+  @Test
+  public void insert() {
+    User john = insertUser("John", "Doe");
+    assertThat(john.getId(), is(1));
+  }
+
+  @Test
+  public void update() {
+    User jane = insertUser("Jane", "Doe");
+    jane.getName().setLastName("Smith");
+
+    UserRecord record = mapper.map(jane, UserRecord.class);
+    db.executeUpdate(record);
+
+    User user = db.selectFrom(USER)
+        .where(USER.LAST_NAME.equal("Smith"))
+        .fetchOne().map(recordMapper);
+    assertThat(user, is(jane));
+  }
+
+  private User insertUser(String firstName, String lastName) {
+    UserRecord record = new UserRecord();
+    record.setFirstName(firstName);
+    record.setLastName(lastName);
+    return db.insertInto(USER).set(record).returning().fetchOne().map(recordMapper);
   }
 }
